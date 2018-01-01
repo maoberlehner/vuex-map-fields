ed1922258ec6e059b7333e75822f7b4ed4b04407
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-01-01 08:01:25 +0100

    Remove .tgz file after fake publishing
    Clean up generated package .tgz file after fake publishing. This is also
    necessary to prevent problems with the test script if multiple .tgz
    files exist.
    
    Fixes #4

--- test/utils/fake-publish.sh
+++ test/utils/fake-publish.sh
@@ -3,6 +3,7 @@
 npm pack ./
 rm -Rf ./test/package
 tar -xzf vuex-map-fields-*.tgz -C ./test
+rm -f vuex-map-fields-*.tgz
 # Rename the fake published package to prevent Jest from
 # complaining about two packages having the same name.
 sed -i -e 's/vuex-map-fields/fake-vuex-map-fields/g' ./test/package/package.json
