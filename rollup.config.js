54c34dea185330da0e30369da883f5c3be12ec30
merceyz <merceyz@users.noreply.github.com>
2020-03-19 15:40:44 +0100

    build: use rollup with multiple outputs and terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,30 @@
 import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
+  input: `./src/index.js`,
+  output: [
+    {
+      file: `./dist/index.js`,
+      format: `umd`,
+      name: `vuex-map-fields`,
+    },
+    {
+      file: `./dist/index.min.js`,
+      format: `umd`,
+      name: `vuex-map-fields`,
+      plugins: [terser()],
+    },
+    {
+      file: `./dist/index.esm.js`,
+      format: `es`,
+    },
+    {
+      file: `./dist/index.esm.min.js`,
+      format: `es`,
+      plugins: [terser()],
+    },
+  ],
   plugins: [
     babel(),
   ],
